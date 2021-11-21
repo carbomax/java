3e1ed097e9cbfd2225584db81e359ee908ba6d5d
Luis <lacarbonel2017@gmail.com>
2021-11-21 17:01:34 -0300

    creating patters file

--- src/main/java/observer/subject/BinaryObserver.java
+++
@@ -1,9 +0,0 @@
-package observer.subject;
-
-public class BinaryObserver extends Observer {
-
-    @Override
-    public void update(Subject subject) {
-        System.out.println( "Octal String: " + Integer.toBinaryString((Integer) subject.getState()) );
-    }
-}
--- src/main/java/observer/subject/Observer.java
+++
@@ -1,5 +0,0 @@
-package observer.subject;
-
-abstract class Observer {
-    public abstract void update(Subject subject);
-}
